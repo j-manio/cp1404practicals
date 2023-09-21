68346c340e1a5354487b0082154856d2b876f5c9
j-manio <john..manio@my.jcu.edu.au>
2023-09-22 05:28:51 +1000

    Rename variable score to valid score

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,4 +1,5 @@
 """" Score Menu Program """
+from prac_02 import score
 
 MENU = ("(G)et valid score\n"
         "(P)rint result\n"
@@ -11,10 +12,10 @@ def main():
     user_choice = str(input(">>>: ").upper())
     while user_choice != "Q":
         if user_choice == "G":
-            score = int(input("Score: "))
-            while score < 0 or score > 100:
+            valid_score = int(input("Score: "))
+            while valid_score < 0 or valid_score > 100:
                 print("Invalid score")
-                score = int(input("Score: "))
+                valid_score = int(input("Score: "))
         elif user_choice == "P":
             pass
         elif user_choice == "S":
@@ -24,5 +25,6 @@ def main():
         print(MENU)
         user_choice = str(input(">>>: ").upper())
     print("Farewell")
+
 
 main()

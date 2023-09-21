5e830cf6e937379340c4d0ddead559bf65e18cd4
j-manio <john..manio@my.jcu.edu.au>
2023-09-22 05:19:29 +1000

    Add error checking

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,1 +1,17 @@
 """" Score Menu Program """
+
+MENU = ("(G)et valid score\n"
+        "(P)rint result\n"
+        "(S)how stars\n"
+        "(Q)uit")
+
+
+def main():
+    print(MENU)
+    user_choice = str(input(">>>: ").upper())
+    while user_choice != "Q":
+        print("Invalid score")
+        user_choice = str(input(">>>: ").upper())
+
+
+main()

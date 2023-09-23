a98fe28cb0c0b31d3118bba3a46aa0546fd8b439
j-manio <john..manio@my.jcu.edu.au>
2023-09-23 20:09:47 +1000

    Fix the determine score function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,14 +2,24 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Pass")
-    elif score < 50:
-        print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    print(determine_result(score))
+
+
+def determine_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    else:
+        if score >= 90:
+            return "Excellent"
+        elif score >= 50:
+            return "Pass"
+        else:
+            return "Bad"
+
+
+main()

5a2dd0e626b5367c283be17acc5d2061d253a7c6
j-manio <john..manio@my.jcu.edu.au>
2023-10-10 16:40:19 +1000

    answered given questions about exceptions

--- prac_03/exception_demo.py
+++ prac_03/exception_demo.py
@@ -2,8 +2,11 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+It will occur if the input is not an integer
 2. When will a ZeroDivisionError occur?
+It will occur if denominator is 0
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+We could use a while loop for error checking, but exception error checking is the best method
 """
 
 try:

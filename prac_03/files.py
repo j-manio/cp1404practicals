3947120a09b02532a4001ee3423713361b07703c
j-manio <john..manio@my.jcu.edu.au>
2023-10-10 19:10:50 +1000

    Add read file code and write file code

---
+++ prac_03/files.py
@@ -0,0 +1,19 @@
+"""
+Pseudocode
+get user name
+open file to write
+store it in text file
+
+open file to read
+print the stored name
+"""
+FILENAME = "name.txt"
+name = input("Name: ")
+out_file = open(FILENAME, 'w')
+print(name, file=out_file)
+out_file.close()
+
+in_file = open(FILENAME, 'r')
+text = in_file.read()
+in_file.close()
+print(text)

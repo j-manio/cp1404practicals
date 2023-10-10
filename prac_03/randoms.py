eec19083ce60d5de3cffcda609d02bed7eb0914b
j-manio <john..manio@my.jcu.edu.au>
2023-10-10 15:12:35 +1000

    Answer questions and wrote a code to produce random number between 1 - 100 inclusive

---
+++ prac_03/randoms.py
@@ -0,0 +1,17 @@
+"""
+print(random.randint(5, 20)) # line 1
+output: 5
+Smallest would be 5 and the largest number is 20
+
+print(random.randrange(3, 10, 2))  # line 2
+output: 9
+Smallest number is 3 and the largest is 9
+It would only produce odd numbers
+
+print(random.uniform(2.5, 5.5))  # line 3
+output: 3.0967114231333763
+The smallest 2.5 and the largest is 5.5
+"""
+import random
+
+print(random.randrange(1, 101))  # Start on 1 and stop on 100

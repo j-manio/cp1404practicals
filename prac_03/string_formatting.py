a2a265f9777ae51a265f3ad7baebadf6fcc41f5b
j-manio <john..manio@my.jcu.edu.au>
2023-10-10 14:42:33 +1000

    Add code using f string to print

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,7 +32,8 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use f-string formatting to produce the output:
+
+print(f"{year} {name} for about ${cost:,.0f}!")
 # 1922 Gibson L-5 CES for about $16,035!
 
 # TODO: Using a for loop with the range function and string formatting,

c62c72c2a7b0c7ed9ef577a31713bedd42213c69
j-manio <john..manio@my.jcu.edu.au>
2023-10-10 23:16:07 +1000

    Remove TODO task

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -35,7 +35,6 @@ def is_valid_password(password):
     count_digit = 0
     count_special = 0
     for char in password:
-        # TODO: count each kind of character (use str methods like isdigit)
         if char.islower():
             count_lower += 1
         elif char.isupper():
@@ -46,10 +45,9 @@ def is_valid_password(password):
             count_special += 1
     print(f"{count_lower} lower, {count_upper} upper, {count_digit} digit, {count_special} special")
 
-    # TODO: if any of the 'normal' counts are zero, return False
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
-    # TODO: if special characters are required, then check the count of those
+
     if SPECIAL_CHARS_REQUIRED:
         if count_special == 0:
             return False

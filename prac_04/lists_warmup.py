0d2e6445868d2fdb59dce30905ba6b0761035c31
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 00:30:15 +1000

    Finish the exercises

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,22 @@
+""" Warm-up """
+
+"""
+
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+numbers[0]  # 3
+numbers[-1]  # 2
+numbers[3]  # 1
+numbers[:-1]  # [3, 2, 4, 1, 5, 9[
+numbers[3:4]  # 1
+5 in numbers  # True
+7 in numbers  # False
+"3" in numbers  # False
+numbers + [6, 5, 3]  # [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+1. numbers[0] = "ten"
+2. numbers[-1] = 1
+3. numbers[2:]
+4. print(9 in numbers)
+"""

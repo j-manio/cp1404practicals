0278799855a1a2765fbbd357a13116e3d05bb775
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 03:24:14 +1000

    Add code get last name if full name is greater than eleven

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -46,3 +46,5 @@ print(greater_than_nine)
 # TODO: (more advanced) use a list comprehension and the join string method
 # to create a string (not list) of the last names for those full names longer than 11 characters
 # the result should be: 'Harlem, Hendrix, Lovelace'
+parts = " ".join(last_name.split()[-1] for last_name in full_names if len(last_name) > 11)
+print(parts)

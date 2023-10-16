b6bf39ea7c3cc7d47a35da71e85a7e513fbc6393
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 02:53:20 +1000

    Add code to display list of string numbers into list of integers

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -35,7 +35,9 @@ print(lowercase_full_names)
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
 # TODO: list comprehension to create a list of integers from the above list of strings
-# numbers =
+numbers = tuple([int(number) for number in almost_numbers])
+print(numbers)
+print(type(numbers[0]))
 
 # TODO: list comprehension to create a list of only the numbers that are
 # greater than 9 from the numbers (not strings) you just created

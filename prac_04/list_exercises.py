d8bceb335a8750339f0846b8ffa251b22a76a8ce
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 02:26:01 +1000

    Add code to print largest number from list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,4 @@ for i in range(5):
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")

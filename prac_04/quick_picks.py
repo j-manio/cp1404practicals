0dd8fa8f8c2c4d5dc26d0e0764b71b715fff4a21
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 04:27:39 +1000

    Add code to produce 6 random numbers in a line

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -2,12 +2,14 @@
 import random
 MIN_VALUE = 1
 MAX_VALUE = 45
+ENTRIES_PER_LINE = 6
 
 num_pick = int(input("How many quick picks? "))
-lines = []
-for i in range(num_pick):
-    line = random.randint(MIN_VALUE, MAX_VALUE)
-    lines.append(line)
-print(lines)
+quick_picks_list = []
+for i in range(ENTRIES_PER_LINE):
+    number = random.randint(MIN_VALUE, MAX_VALUE)
+    quick_picks_list.append(number)
+print(quick_picks_list)
+
 
 

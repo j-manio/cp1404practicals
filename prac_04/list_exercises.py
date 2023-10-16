313686e6a8882ecd35ac1159ac9a16946cb90818
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 02:38:40 +1000

    Add code to check username if in the list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -15,3 +15,11 @@ print(f"The average of the numbers is {average}")
 
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+

af40bb3805730fe5b04fab16f3c4f9787dce560d
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 03:58:29 +1000

    Add code to display random numbers same with how many user wants

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,12 @@
+"""Quick picks - Lottery Ticket Generator"""
+import random
+MIN_VALUE = 1
+MAX_VALUE = 45
+
+num_pick = int(input("How many quick picks? "))
+lines = []
+for i in range(num_pick):
+    line = random.randint(MIN_VALUE, MAX_VALUE)
+    print(line)
+
+

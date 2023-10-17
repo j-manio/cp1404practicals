47c9a18ae454fd555bed4a9a0f89602ec4d8ed5f
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 22:11:38 +1000

    Add for loop to format list to string

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,5 +1,6 @@
 """Quick picks - Lottery Ticket Generator"""
 import random
+
 MIN_VALUE = 1
 MAX_VALUE = 45
 ENTRIES_PER_LINE = 6
@@ -10,8 +11,10 @@ for i in range(num_pick):
     quick_picks_list = []
     for j in range(ENTRIES_PER_LINE):
         number = random.randint(MIN_VALUE, MAX_VALUE)
+        while number in quick_picks_list:
+            number = random.randint(MIN_VALUE, MAX_VALUE)
         quick_picks_list.append(number)
-    quick_picks_list.sort()
-    print(quick_picks_list)
-
-
+        quick_picks_list.sort()
+    for l in range(ENTRIES_PER_LINE):
+        print(f"{quick_picks_list[l]:>2}", end=" ")
+    print()

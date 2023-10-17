8a8424b7d682216be112532dfd3a47d73576df53
j-manio <john..manio@my.jcu.edu.au>
2023-10-17 21:28:23 +1000

    Add code to display random numbers to rows and columns

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -5,11 +5,13 @@ MAX_VALUE = 45
 ENTRIES_PER_LINE = 6
 
 num_pick = int(input("How many quick picks? "))
-quick_picks_list = []
-for i in range(ENTRIES_PER_LINE):
-    number = random.randint(MIN_VALUE, MAX_VALUE)
-    quick_picks_list.append(number)
-print(quick_picks_list)
 
+for i in range(num_pick):
+    quick_picks_list = []
+    for j in range(ENTRIES_PER_LINE):
+        number = random.randint(MIN_VALUE, MAX_VALUE)
+        quick_picks_list.append(number)
+    quick_picks_list.sort()
+    print(quick_picks_list)
 
 

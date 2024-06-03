d32ddfdd543cd2092c69c4fbbd8cd37b3f244067
j-manio <john..manio@my.jcu.edu.au>
2024-06-03 23:31:01 +1000

    Add pseudocode to file

---
+++ prac_02/score_menu.py
@@ -0,0 +1,22 @@
+"""Score Menu Program"""
+"""
+Pseudocode:
+display menu
+get user choice from the menu
+choice should be valid and if not it will show an error message
+
+if user choose G, it will prompt user to input a valid score
+this will error check it if the valid score is within 0 to 100
+
+if user choose P, it will print the result of the given score
+
+if user choose S, it will print stars equals to valid score
+
+Q is to quit the program and print a farewell message
+
+"""
+
+MENU = ("(G)et valid score\n"
+        "(P)rint result\n"
+        "(S)how stars\n"
+        "(Q)uit")

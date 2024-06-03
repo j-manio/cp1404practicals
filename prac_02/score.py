ec4503dcc527a10612197c9ffeaf6df41d575779
j-manio <john..manio@my.jcu.edu.au>
2024-06-03 23:15:12 +1000

    Improved score checker program

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,24 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+# score = float(input("Enter score: "))
+# if score < 0 or score > 100:
+#     print("Invalid score")
+# else:
+#     if score >= 90:
+#         print("Excellent")
+#     elif score >= 50:
+#         print("Pass")
+#     elif score < 50:
+#         print("Bad")
+
 score = float(input("Enter score: "))
+
 if score < 0 or score > 100:
     print("Invalid score")
+elif score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Pass")
 else:
-    if score >= 90:
-        print("Excellent")
-    elif score >= 50:
-        print("Pass")
-    elif score < 50:
-        print("Bad")
+    print("Bad")

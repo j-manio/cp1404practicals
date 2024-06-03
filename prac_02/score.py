417e4b2135328a7c6e170786894d6b8dc4a7a80d
j-manio <john..manio@my.jcu.edu.au>
2024-06-03 23:23:51 +1000

    Refactor score checker program to function

--- prac_02/score.py
+++ prac_02/score.py
@@ -14,13 +14,27 @@ Broken program to determine score status
 #     elif score < 50:
 #         print("Bad")
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Bad")
+def main():
+    score = get_score()
+    check_score(score)
+    print(check_score(score))
+
+
+def check_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Pass"
+    else:
+        return "Bad"
+
+
+def get_score():
+    score = float(input("Enter score: "))
+    return score
+
+
+main()

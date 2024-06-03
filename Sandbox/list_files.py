06554340c3920cae63de7bc69faace16f563e736
j-manio <113533272+j-manio@users.noreply.github.com>
2024-06-04 00:05:03 +1000

    Delete Sandbox directory

--- Sandbox/list_files.py
+++
@@ -1,7 +0,0 @@
-import os
-
-print(f"The files and folders in {os.getcwd()} are:")
-items = os.listdir('.')
-for item in items:
-    prefix = "(d) " if os.path.isdir(item) else "(f) "
-    print(f"{prefix}\t{item}")
--- Sandbox/my_name.py
+++
@@ -1,1 +0,0 @@
-""" John Ervin Manio """

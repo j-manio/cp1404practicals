226b7ca77f63241723431213b38d0ae798057a91
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 23:15:51 +1000

    Add code to display content from text file

--- prac_03/files.py
+++ prac_03/files.py
@@ -16,3 +16,6 @@ write code that opens "name.txt" and reads the name (as above) then prints (note
 Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
 Use open and close for this question.
 """
+in_file = open(FILENAME, "r")
+print(in_file.read())
+in_file.close()

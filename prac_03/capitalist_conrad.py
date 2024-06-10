e6f8d2b4a28837961384f1e632f95c24cef49e77
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 21:54:43 +1000

    Refactor constants

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,14 +9,15 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1
+MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 number_of_days = 0
 price = INITIAL_PRICE
 print(f"Starting price: ${price:,.2f}")
+
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0

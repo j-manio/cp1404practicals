c9ef21b33f11e275c8d964c095ba09c9e8c6de33
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 22:35:49 +1000

    Add code to check password length

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -28,14 +28,15 @@ def main():
 def is_valid_password(password):
     """Determine if the provided password is valid."""
     # TODO: if length is wrong, return False
-
+    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
+        return False
     number_of_lower = 0
     number_of_upper = 0
     number_of_digit = 0
     number_of_special = 0
     for character in password:
         # TODO: count each kind of character (use str methods like isdigit)
-        pass
+       pass
 
     # TODO: if any of the 'normal' counts are zero, return False
 

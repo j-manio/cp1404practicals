7e0b207647da96a12e8fc47b935d1dc7dacf46c0
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 22:24:25 +1000

    Add answers to questions

--- prac_03/exception_demo.py
+++ prac_03/exception_demo.py
@@ -2,10 +2,13 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+When an input is not an integer i.e. strings
 2. When will a ZeroDivisionError occur?
+It will occur when the denominator is zero
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+Using while loop can keep asking the user if they input a 0 as the denominator (Infinite loop).
 """
-
+# while True:
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))

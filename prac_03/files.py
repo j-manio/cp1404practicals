0a24b325319a505a7e977353be6d79d3722e3d23
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 23:15:28 +1000

    Add code to save output to text file

---
+++ prac_03/files.py
@@ -0,0 +1,18 @@
+"""
+1. Write code that asks the user for their name,
+then opens a file called name.txt and writes that name to it.
+Use open and close for this question.
+"""
+FILENAME = "name.txt"
+
+name = input("Name: ")
+out_file = open(FILENAME, "w")
+print(name, file=out_file)
+out_file.close()
+
+"""
+In the same file, but as if it were a separate program, 
+write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
+Hi Bob! (or whatever the name is in the file). Do not simply print the user's input variable!
+Use open and close for this question.
+"""

28e1ab33b9b824f8d7d37637c99a5a8f65d144e8
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 22:38:27 +1000

    Add code to count each character in password

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -27,7 +27,6 @@ def main():
 
 def is_valid_password(password):
     """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
     if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
         return False
     number_of_lower = 0
@@ -35,8 +34,14 @@ def is_valid_password(password):
     number_of_digit = 0
     number_of_special = 0
     for character in password:
-        # TODO: count each kind of character (use str methods like isdigit)
-       pass
+        if character.islower():
+            number_of_lower += 1
+        elif character.isupper():
+            number_of_upper += 1
+        elif character.isdigit():
+            number_of_digit += 1
+        elif character in SPECIAL_CHARACTERS:
+            number_of_special += 1
 
     # TODO: if any of the 'normal' counts are zero, return False
 

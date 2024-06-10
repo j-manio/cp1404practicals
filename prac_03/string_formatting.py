0f4593797fda654170c8755080f5382c88d31896
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 21:06:47 +1000

    Add text formatting to align characters

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,8 +32,9 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use f-string formatting to produce the output:
+# Use f-string formatting to produce the output:
 # 1922 Gibson L-5 CES for about $16,036!
+print(f"{year} {name} for about ${cost:,.0f}!")
 
 # TODO: Using a for loop with the range function and f-string formatting,
 # produce the following right-aligned output (DO NOT use a list):
@@ -48,3 +49,5 @@ for i, number in enumerate(numbers, 1):
 # 2 ^ 8 is  256
 # 2 ^ 9 is  512
 # 2 ^10 is 1024
+for i in range(11):
+    print(f"2 ^ {i+1:<2} is {i ** 2:<10}")

ea9a5a2a3c5df0b3abd8a7eef8e29d89f00c1a6c
j-manio <john..manio@my.jcu.edu.au>
2024-06-10 22:01:27 +1000

    Add text file to record output

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,6 +9,7 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
+FILENAME = "prices.txt"
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1
@@ -16,9 +17,9 @@ MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 number_of_days = 0
 price = INITIAL_PRICE
-print(f"Starting price: ${price:,.2f}")
 
-
+out_file = open(FILENAME, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
     # generate a random integer of 1 or 2
@@ -33,4 +34,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
     number_of_days += 1
     price *= (1 + price_change)
-    print(f"On day {number_of_days} ${price:,.2f}")
+    print(f"On day {number_of_days} ${price:,.2f}", file=out_file)
+
+out_file.close()

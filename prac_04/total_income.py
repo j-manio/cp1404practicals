6f6b6e2f1762dedaec1b6dc24fa5866d939075d2
j-manio <john..manio@my.jcu.edu.au>
2024-06-16 22:03:36 +1000

    Changed the print statement to use fstring

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -23,7 +23,8 @@ def print_report(incomes, number_of_months):
     for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        # print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print(f"Month {month:2} - Income: ${income:10.2f}     Total: ${total:10.2f}")
 
 
 main()

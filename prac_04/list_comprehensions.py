91dfef97cc264ba961f6e219610437492bf82c5d
j-manio <john..manio@my.jcu.edu.au>
2024-06-16 23:22:37 +1000

    Add code to display lowercase full names

--- prac_04/list_comprehensions.py
+++ prac_04/list_comprehensions.py
@@ -29,11 +29,13 @@ print(a_names)
 # 'Ada Alan Angel Bob Jimi'
 print(" ".join(sorted(names)))
 
-# TODO: list comprehension to create a list of all the full_names in lowercase format
-# lowercase_full_names =
+# List comprehension to create a list of all the full_names in lowercase format
+lowercase_full_names = [full_name.lower() for full_name in full_names]
+print(lowercase_full_names)
+
 
 almost_numbers = ['0', '10', '21', '3', '-7', '88', '9']
-# TODO: list comprehension to create a list of integers from the above list of strings
+# List comprehension to create a list of integers from the above list of strings
 # numbers =
 
 # TODO: list comprehension to create a list of only the numbers that are

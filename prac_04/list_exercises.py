7e516310e88171ba63b756a81bf63b5a046c9f7e
j-manio <john..manio@my.jcu.edu.au>
2024-06-16 23:07:22 +1000

    Added code to validate username in list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -14,3 +14,12 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average number is {sum(numbers)/len(numbers)}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter a username: ")
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")

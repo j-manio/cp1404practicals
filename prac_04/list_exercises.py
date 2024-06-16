4e03a0acd7a9ae99b5a7746561022404863a23ce
j-manio <john..manio@my.jcu.edu.au>
2024-06-16 23:03:46 +1000

    Add print statements to display multiple outputs

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -7,4 +7,10 @@ You can use the functions min, max, sum and len, and you can use the append meth
 numbers = []
 for i in range(5):
     number = input("Enter a number: ")
-    numbers.append(number)
+    numbers.append(int(number))
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers)/len(numbers)}")

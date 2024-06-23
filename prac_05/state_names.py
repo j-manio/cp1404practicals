78e1045df316b1c8b4cf2f49de28cf6952221512
j-manio <john..manio@my.jcu.edu.au>
2024-06-23 21:16:43 +1000

    Added exception code to handle errors

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -8,10 +8,17 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ").upper()
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ")
+is_valid_input = False
+while is_valid_input:
+    try:
+        state_code = str(input("Enter short state: ").upper())
+        if state_code in CODE_TO_NAME:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+            is_valid_input = True
+        else:
+            print("Invalid state code")
+    except KeyError:
+        print("State code not found in the dictionary")
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:3} is {name}")

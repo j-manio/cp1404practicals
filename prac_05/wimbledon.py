86aa2815d3d767877bb3c42b12d52fe766828ae6
j-manio <john..manio@my.jcu.edu.au>
2024-06-23 23:31:35 +1000

    Added function to load data file

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -3,3 +3,28 @@ Wimbledon
 Estimate: 30 minutes
 Actual:
 """
+
+FILENAME = "wimbledon.csv"
+
+
+def main():
+
+    champion_to_count = {}
+    records = load_data()
+    for record in records:
+
+
+
+
+def load_data():
+    """Load data file"""
+    records = []
+    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove header
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+main()

649edcce5940632dc3058529103a2dad3892480f
j-manio <john..manio@my.jcu.edu.au>
2024-06-23 23:12:39 +1000

    Implemented name confirmation program

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,9 +5,27 @@ Actual:
 """
 
 
-email = input('Enter your email: ')
-while email != "":
+def main():
+    email_to_name = {}
+    email = input('Enter your email: ')
+    while email != "":
+        name = extract_name(email)
+        confirmation = input(f'Is your name: {name} Y/N?: ').upper()
+        if confirmation != 'Y' or confirmation == "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input('Enter your email: ')
+
+    for email, name in email_to_name.items():
+        print(f"{name}: ({email})")
+
+
+def extract_name(email):
+    """Extract name from email"""
     address_sign = email.split('@')[0]
     parts = address_sign.split(".")
     name = " ".join(parts).title()
-    email = input('Enter your email: ')
+    return name
+
+
+main()

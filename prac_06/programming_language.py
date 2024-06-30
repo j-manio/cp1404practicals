45686ae85faa89020371f64a64c0ef51512e11f2
j-manio <john..manio@my.jcu.edu.au>
2024-06-30 23:13:38 +1000

    Implemented code modification

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection="", year=0):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic".title()
+
+
+
+
+
+
